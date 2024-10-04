18ce95fe501a7c6f07feaf430c6eb06059eb88cd
Micael <cardona.micael@ficct.uagrm.edu.bo>
2024-10-04 02:57:28 -0400

    socket en uno solo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import router from "./routes/routes.js";
 import http from 'http';
 import { Server as socketio } from 'socket.io'; // Socket.IO con import
@@ -9,23 +9,23 @@ export class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
-    this.portSocket = process.env.PORT_SOCKET;
-    this.server = http.createServer(this.app);
+    this.port = process.env.PORT || 3001; // Usamos un solo puerto
+    this.server = http.createServer(this.app); // Usamos el servidor HTTP compartido
     this.routerMain = "/api-v1"; 
-    //configuracion del socket
 
+    // Configuración de Socket.IO
     this.io = new socketio(this.server, {
       cors: {
         origin: "http://localhost:3000", // La URL del cliente React
         methods: ["GET", "POST"]
       }
     });
-  } 
+  }
+
   middlewares() {
     this.app.use(cors());
     this.app.use(express.json());
-    this.app.use("/api-v1", router);
+    this.app.use(this.routerMain, router);
   }
 
   configureSockets() {
@@ -35,17 +35,10 @@ export class Server {
   execute() {
     this.middlewares();
     this.configureSockets();
-    this.server.listen(this.portSocket, () => {
-      console.log(`Socket.IO server is running on port ${this.portSocket}`);
-    });
 
-    // Escuchar en el puerto configurado para Express o 3001 por defecto
-    this.app.listen(this.port || 3001, () => {
-      let message = "Server is running on port " + (this.port || 3001);
-      console.log(message);
+    // Iniciamos el servidor HTTP que manejará tanto la API como los WebSockets
+    this.server.listen(this.port, () => {
+      console.log(`Server and Socket.IO running on port ${this.port}`);
     });
   }
-
-
 }
-
